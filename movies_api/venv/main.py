cc09d92f2e2dd09ec6fae9859ff31bf00bbf4905
Joel ML <104096752+JoelML88@users.noreply.github.com>
2023-12-10 17:56:19 -0600

    starting

---
+++ movies_api/venv/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+app.title = "Mi app de prueba con FastAPI"
+app.version = "0.0.1"
+
+movies = [
+    {
+        "id":1,
+        "title": "Avatar",
+        "Overview": "monos azules peleando",
+        "year": 2009,
+        "rating": 7.8,
+        "category": "Accion",
+
+    },
+    {
+        "id":2,
+        "title": "XXX",
+        "Overview": "monos",
+        "year": 20015,
+        "rating": 9.8,
+        "category": "Por",
+    }
+]
+
+
+@app.get('/', tags=["home"])
+
+def message():
+    return HTMLResponse('<h1>Hey prro</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
